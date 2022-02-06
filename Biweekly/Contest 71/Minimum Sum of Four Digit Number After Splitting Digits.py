edb003ac7d603e58639a71d8026df14d61c2ce49
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2022-02-06 09:07:11 +0530

    Add files via upload

--- Biweekly/Contest 71/Minimum Sum of Four Digit Number After Splitting Digits.py
+++ Biweekly/Contest 71/Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        nums1=[int(x) for x in str(num)]
+        nums1.sort()
+        a=0
+        b=0
+        for i in range(len(nums1)):
+            if(i%2!=0):
+                a=a*10+nums1[i]
+            else:
+                b=b*10+nums1[i]
+        return a+b
+        
