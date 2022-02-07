5590049dab00b6294a3d4e89ab90dcf81afc0a65
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2022-02-07 13:22:36 +0530

    Create Partition Array According to Given Pivot.py

--- Biweekly/Contest 71/Partition Array According to Given Pivot.py
+++ Biweekly/Contest 71/Partition Array According to Given Pivot.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        s=[]
+        s1=[]
+        s2=[]
+        for i in nums:
+            if(i<pivot):
+                s.append(i)
+            elif(i==pivot):
+                s1.append(i)
+            elif(i>pivot):
+                s2.append(i)
+        return s+s1+s2
