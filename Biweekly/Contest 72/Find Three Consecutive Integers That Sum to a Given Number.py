d4908449e22045cb619f7a91f7890846ead1fd77
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2022-02-20 15:45:04 +0530

    Update Find Three Consecutive Integers That Sum to a Given Number.py

--- Biweekly/Contest 72/Find Three Consecutive Integers That Sum to a Given Number.py
+++ Biweekly/Contest 72/Find Three Consecutive Integers That Sum to a Given Number.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def sumOfThree(self, num: int) -> List[int]:
+        s=[]
+        if(num%3==0):
+            s.append(int(num/3 -1))
+            s.append(int(num/3))
+            s.append(int(num/3 +1))
+        return s
+        
