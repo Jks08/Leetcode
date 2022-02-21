b8385272ef7428a1f3f4c9fd10bd778aa9679ac0
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2022-02-21 11:14:14 +0530

    Update Maximum Split of Positive Even Integers.py

--- Biweekly/Contest 72/Maximum Split of Positive Even Integers.py
+++ Biweekly/Contest 72/Maximum Split of Positive Even Integers.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def maximumEvenSplit(self, finalSum: int) -> List[int]:
+        if finalSum % 2: return []
+        value = int(sqrt(finalSum))
+        ans = [2 * i for i in range(1, value)]    
+        diff = finalSum - sum(ans)
+        if ans and diff <= ans[-1]:
+            return ans[:-1] + [diff + ans[-1]]
+        else:    
+            return ans + [diff]
