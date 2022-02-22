adb28f5fe0066c51542d33e9379b3662f205e05f
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2022-02-22 10:23:22 +0530

    Update Count Good Triplets in an Array.py

--- Biweekly/Contest 72/Count Good Triplets in an Array.py
+++ Biweekly/Contest 72/Count Good Triplets in an Array.py
@@ -1,1 +1,14 @@
+import bisect 
 
+class Solution:
+    def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        dic = {v: i for i, v in enumerate(nums2)}
+        lst = [dic[v] for v in nums1] 
+        res = 0
+        stack = []
+        l = len(lst)
+        for i, v in enumerate(lst):
+            idx = bisect.bisect(stack, v)
+            res += idx * (l - v - (i - idx) - 1)
+            bisect.insort(stack, v)
+        return res
