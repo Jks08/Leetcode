f27b789001772fe3524098f75c6a779ed6a1c7e8
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2022-02-28 09:10:58 +0530

    Update Merge Nodes in Between Zeros.py

--- Weekly/Contest 281/Merge Nodes in Between Zeros.py
+++ Weekly/Contest 281/Merge Nodes in Between Zeros.py
@@ -1,1 +1,19 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr=head.next
+        prev=head
+        while curr:
+            s=0
+            while curr.val!=0:
+                s+=curr.val
+                curr=curr.next
+            prev.next.val=s
+            prev.next.next=curr.next
+            prev=curr
+            curr=curr.next
+        return head.next
